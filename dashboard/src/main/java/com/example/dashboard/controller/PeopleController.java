f66ced77e0928cad0a45c7749d413bdcefbace07
Alvi <alvi@alvi.com>
2020-09-01 14:10:48 +0200

    spring changes

--- dashboard/src/main/java/com/example/dashboard/controller/PeopleController.java
+++ dashboard/src/main/java/com/example/dashboard/controller/PeopleController.java
@@ -5,16 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.dashboard.model.People;
 import com.example.dashboard.service.PeopleService;
@@ -24,26 +15,27 @@ public class PeopleController {
 	
 	@Autowired
 	private PeopleService peopleService;
-	@CrossOrigin(origins = "http://lcalhost:4200")
-	@RequestMapping(value="/create",method=RequestMethod.POST)
+
+	@CrossOrigin
+	@PostMapping("/create")
 	public String create(@RequestBody People people) {
 		People p = peopleService.create(people);
 		return p.toString();
 	}
-	
 
-	@RequestMapping(value="/getAll" , method=RequestMethod.GET)
+	@CrossOrigin
+	@GetMapping("/getAll")
 	public List<People> getAll(){
 		return peopleService.getAll();
 	}
 
-
-	@RequestMapping ("/deleteAll")
+	@CrossOrigin
+	@DeleteMapping("/deleteAll")
 	public String deleteAll() { 
 		peopleService.deleteAll();
 		return "Deleted all records";
 	}
-	 
+	@CrossOrigin
 	@DeleteMapping("/delete/{id}")
 	public ResponseEntity<HttpStatus> deleteTutorial(@PathVariable("id") String id) {
 	  try {
